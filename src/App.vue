86fbed44b21bb25fa725680f54fe0a928f574d91
Pedro Martinez <pmartinez1085@gmail.com>
2017-01-31 16:59:03 +0100

    sync2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+
+    <weather></weather>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Weather from './components/Weather'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Weather
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+ -moz-box-sizzing:
 }
+  body {
+  font-family: Roboto, Arial;
+  font-size: 1.2em;
+  color: #888;
+  }
+
 </style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="weather">
+    <div class="location">{{location}}</div>
+    <div class="date">{{date}}</div>
+    <div class="description">{{description}}</div>
+    <div class="current">
+      <div class="visual">
+        <div class="cloud"></div>
+          <div class="temp">{{temp}}</div>
+        <div class="scale">º</div>
+      </div>
+      <div class="text">
+        <div class="precipitation">Precipitation: {{precipitation}}</div>
+        <div class="humidity">humidity: {{humidity}}</div>
+        <div class="wind">wind: {{wind}}</div>
+        <div class="pollen">pollen: {{pollen}}</div>
+      </div>
+    </div>
+    <div class="forecast">
+    <div class="forecast-day" v-for="forecast in forecasts">
+      <div class="date">{{forecast.date}}</div>
+      <div class="icon">{{forecast.icon}}</div>
+      <div class="low-temp">{{forecast.low-temp}}</div>
+      <div class="high-temp">{{forecast.high-temp}}</div>
+
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'weather',
+  data () {
+    return {
+      location: 'New York, NY',
+      date: 'Tuesday, April 15th',
+      description: ' Description  here',
+      temp: 25,
+      humidity: 97,
+      wind: '4mph SW',
+      pollen: 36,
+      forecasts: [
+        {'date': 'Monday', 'icon': 'cloud', 'low-temp': '28', 'high-temp': '45'},
+        {'date': 'Tuesday', 'icon': 'sun', 'low-temp': '28', 'high-temp': '45'},
+        {'date': 'Wednesday', 'icon': 'sun-cloud', 'low-temp': '28', 'high-temp': '45'},
+        {'date': 'Thursday', 'icon': 'cloud', 'low-temp': '28', 'high-temp': '45'},
+        {'date': 'Friday', 'icon': 'raining', 'low-temp': '28', 'high-temp': '45'},
+        {'date': 'Saturday', 'icon': 'cloud', 'low-temp': '28', 'high-temp': '45'},
+        {'date': 'Sunday', 'icon': 'sun', 'low-temp': '28', 'high-temp': '45'}
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .weather {
+    padding:20px;
+  }
+  .location {
+    font-size: 3em;
+  }
+  date {
+    font-size: 1.5em;
+  }
+  .description {
+    font-size: 1.1em;
+  }
+  .current {
+  overflow: auto;
+  width: 100%;
+
+  }
+  .visual {
+    float: left;
+    width: 40%;
+  }
+  .text {
+    width: 50%;
+    float: right;
+
+  }
+
+  .forecast-dat {
+    display: inline-block;
+  }
+</style>
